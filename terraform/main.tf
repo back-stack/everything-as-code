1b78c7085a496b99a182f46532bd5748779cfd00
Nicholas Morey <nicholas@morey.tech>
2024-03-01 22:05:18 +0000

    feat: add eks cluster
    Signed-off-by: Nicholas Morey <nicholas@morey.tech>

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ data "aws_availability_zones" "available" {}
 locals {
   name = "everything-as-code"
   region = var.region
+  cluster_version = "1.29"
 
   vpc_cidr = var.vpc_cidr
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
